df6c052cca4ca2cab74a80195fdd20b04fc98afe
Tim Kral <timkral@gmail.com>
2014-02-13 11:02:58 -0800

    Improve configuration setup and environment verification via horn

--- heimdall/setup/heimdallconfig.py
+++ heimdall/setup/heimdallconfig.py
@@ -11,25 +11,40 @@ class HeimdallConfig:
     def __init__(self, config_dir, config_filename='.heimdall.config'):
         self.config_file = os.path.join(config_dir, config_filename)
 
-    def init(self):
-        config_dict = {}
+    def exists(self):
+        return os.path.exists(self.config_file)
 
-        config_dict.update(couch_db_instance='{0}'.format(self.__config_ask('CouchDB instance', default_value='http://127.0.0.1:5984')))
-        config_dict.update(git_repo_instance='{0}'.format(self.__config_ask('Git repository instance', default_value='https://git.soma.salesforce.com')))
-        config_dict.update(git_repo_org='{0}'.format(self.__config_ask('Git repository org')))
-        config_dict.update(git_repo_name='{0}'.format(self.__config_ask('Git repository name')))
-        config_dict.update(git_repo_branch='{0}'.format(self.__config_ask('Git repository branch', default_value='master')))
-        config_dict.update(git_repo_access_token='{0}'.format(self.__config_ask('Git repository access token', required=False)))
-        config_dict.update(git_repo_local_dir='{0}'.format(self.__config_ask('Git repository local directory', default_value=os.path.join(os.path.expanduser('~'), '.heimdall'))))
+    def init(self):
+        curr_config_dict = {}
+        if self.exists():
+            curr_config_dict = self.__load()
+
+        # If a configuration already exists then use the current
+        # configuration as default values
+        self.config_dict = {}
+        self.config_dict.update(couch_db_instance='{0}'.format(self.__config_ask('CouchDB instance', curr_config_dict.get('couch_db_instance', 'http://127.0.0.1:5984'))))
+        self.config_dict.update(git_repo_instance='{0}'.format(self.__config_ask('Git repository instance', curr_config_dict.get('git_repo_instance', 'https://git.soma.salesforce.com'))))
+        self.config_dict.update(git_repo_org='{0}'.format(self.__config_ask('Git repository org', curr_config_dict.get('git_repo_org', ''))))
+        self.config_dict.update(git_repo_name='{0}'.format(self.__config_ask('Git repository name', curr_config_dict.get('git_repo_name', ''))))
+        self.config_dict.update(git_repo_branch='{0}'.format(self.__config_ask('Git repository branch', curr_config_dict.get('git_repo_branch', 'master'))))
+        self.config_dict.update(git_repo_access_token='{0}'.format(self.__config_ask('Git repository access token', curr_config_dict.get('git_repo_access_token', ''), required=False)))
+        self.config_dict.update(git_repo_local_dir='{0}'.format(self.__config_ask('Git repository local directory', curr_config_dict.get('git_repo_local_dir', os.path.join(os.path.expanduser('~'), '.heimdall')))))
 
         with open(self.config_file, 'w') as f:
-            json.dump(config_dict, f)
+            json.dump(self.config_dict, f)
 
-    def __config_ask(self, message, required=True, default_value=''):
+    def __config_ask(self, message, default_value, required=True):
         # TODO: Raise exception is len(value) == 0 and required and default_value not present
         value = raw_input('{0}[{1}]: '.format(message, default_value))
         return value if len(value) > 0 else default_value
 
     def load(self):
+        self.config_dict = self.__load()
+
+    def __load(self):
         with open(self.config_file, 'r') as f:
-            self.config_dict = json.load(f)
+            return json.load(f)
+
+    def purge(self):
+        if self.exists():
+            os.remove(self.config_file)
--- heimdall/setup/heimdallverifier.py
+++ heimdall/setup/heimdallverifier.py
@@ -3,38 +3,26 @@ A class for verifying an environment to run Heimdall
 """
 __author__ = 'tkral'
 
-import os
+import requests
 import sys
 
-from heimdall.setup.heimdallconfig import HeimdallConfig
+from heimdall.dataload.couchdbclient import CouchDBClient
+from heimdall.dataload.gitdataloader import RemoteGitRepo
 from subprocess import check_output
 
 class HeimdallVerifier:
 
-    def __init__(self, config_dir):
-        self.config_dir = config_dir
+    def __init__(self, config):
+        self.config = config
 
     def verify(self):
-        sys.stdout.write('Verifying heimdall config exists...')
-        config = HeimdallConfig(self.config_dir)
-        self.__verify_bool(os.path.exists(config.config_file))
+        self.__verify_config_exists()
+        self.__verify_config_loading()
+        self.__verify_git_version('1.7')
+        self.__verify_ccollab_version('v6.5')
+        self.__verify_couchdb_connection()
+        self.__verify_git_connection()
 
-        sys.stdout.write('Verifying git version >= 1.7...')
-        git_version_output = check_output(['git', '--version'])
-        git_version_parsed = git_version_output.split()
-        git_version_number = git_version_parsed[2]
-        sys.stdout.write('[{0}]'.format(git_version_number))
-        self.__verify_bool(git_version_number > '1.7')
-
-        sys.stdout.write('Verifying ccollab version >= v6.5...')
-        ccollab_version_output = check_output(['ccollab', '--version'])
-        ccollab_version_parsed = ccollab_version_output.split()
-        ccollab_version_number = ccollab_version_parsed[1]
-        sys.stdout.write('[{0}]'.format(ccollab_version_number))
-        self.__verify_bool(ccollab_version_number > 'v6.5')
-
-        # TODO: Verify connection to couch_db_instance
-        # TODO: Verify connection to git_repository_instance
         # TODO: Verify connection using ccollab
 
     def __verify_bool(self, bool):
@@ -42,3 +30,67 @@ class HeimdallVerifier:
             print '[OK]'
         else:
             print '[ERROR]'
+
+    def __verify_ccollab_version(self, expected_ccollab_version):
+        sys.stdout.write('Verifying ccollab version >= {0}...'.format(expected_ccollab_version))
+        ccollab_version_output = check_output(['ccollab', '--version'])
+        ccollab_version_parsed = ccollab_version_output.split()
+        ccollab_version_number = ccollab_version_parsed[1]
+        sys.stdout.write('[{0}]'.format(ccollab_version_number))
+        self.__verify_bool(ccollab_version_number > expected_ccollab_version)
+
+    def __verify_config_exists(self):
+        sys.stdout.write('Verifying heimdall configuration exists...')
+        self.__verify_bool(self.config.exists())
+
+    def __verify_config_loading(self):
+        sys.stdout.write('Verifying heimdall configuration loading...')
+        self.config_loaded = False
+        if not self.config.exists():
+            print '[SKIP]'
+        else:
+            try:
+                self.config.load()
+                self.config_loaded = True
+            except Exception as e:
+                pass
+
+            self.__verify_bool(self.config_loaded)
+
+    def __verify_couchdb_connection(self):
+        sys.stdout.write('Verifying CouchDB connection...')
+        if not self.config_loaded:
+            print '[SKIP]'
+        else:
+            try:
+                # At initialization, the CouchDBClient attempts to
+                # get a handle to the heimdall data store. So if this
+                # is successful, we're good. Otherwise, we're bad.
+                CouchDBClient(self.config)
+                print '[OK]'
+            except Exception as e:
+                print '[ERROR]'
+
+    def __verify_git_connection(self):
+        sys.stdout.write('Verifying remote git connection...')
+        if not self.config_loaded:
+            print '[SKIP]'
+        else:
+            remote_git_repo = RemoteGitRepo(self.config)
+            get_response = requests.get(remote_git_repo.remote_api_url)
+            if get_response.status_code != 200:
+                print '[ERROR]'
+            else:
+                get_response_dict = get_response.json()
+                if 'Not Found' in get_response_dict.values():
+                    print '[ERROR]'
+                else:
+                    print '[OK]'
+
+    def __verify_git_version(self, expected_git_version):
+        sys.stdout.write('Verifying git version >= {0}...'.format(expected_git_version))
+        git_version_output = check_output(['git', '--version'])
+        git_version_parsed = git_version_output.split()
+        git_version_number = git_version_parsed[2]
+        sys.stdout.write('[{0}]'.format(git_version_number))
+        self.__verify_bool(git_version_number > expected_git_version)
